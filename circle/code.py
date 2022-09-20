910592662a60ff9d36ae69f4bb233a3b0a9d9bcc
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2022-09-20 20:31:58 +0300

    Create code.py

---
+++ circle/code.py
@@ -0,0 +1,9 @@
+from math import pi
+
+default_radius = 10
+
+def circle_perimeter(radius=default_radius):
+  return 2 * pi * radius
+
+def circle_area(radius=default_radius):
+  return pi * radius ** 2
