890ec8b136e60991ab62446b2e4aae869cccd362
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2022-09-20 21:22:33 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .circle import circle_perimeter, circle_area
+from .triangle import triangle_perimeter, triangle_area
+from .square import square_perimeter, square-area
+
+__all__ = (
+           'circle_perimeter', 'circle_area',
+           'triangle_perimeter', 'triangle_area',
+           'square_perimeter', 'square-area'
+          )
